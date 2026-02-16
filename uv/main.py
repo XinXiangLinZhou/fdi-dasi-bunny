d2d054930e4a52adb1cbbcc17d60af031bb8202a
XinXiangLinZhou <xinlin@ucm.es>
2026-02-16 20:37:54 +0100

    get informacion

--- uv/main.py
+++ uv/main.py
@@ -1,6 +1,45 @@
+import requests
+#from fastapi import FastAPI
 def main():
     print("Hello from fdi-dasi-bunny!")
 
+
+#app = FastAPI()
+SERVER_URL = "http://147.96.81.252:7719/"
+
+
+# @app.on_event("startup")
+# def startup_event():
+#     requests.post(
+#         f"{SERVER_URL}/agents/register",
+#         json={
+#             "agent_id": "agente_01",
+#             "tipo": "backend"
+#         }
+#     )
+
+
+#post name
+
+#name=requests.post(SERVER_URL+"alias/bunny")
+name="bunny"
+#get info
+info=requests.get(SERVER_URL+"info")
+informacion=info.json()
+recursos=informacion["Recursos"]
+objetivo=informacion["Objetivo"]
+#print(recursos["queso"],objetivo)
+#get gente
+gente=requests.get(SERVER_URL+"gente")
+personas=gente.json()
+jugadores={}
+jugadores={p["alias"]:p["ip"] for p in personas if p["alias"] != name}
+print(jugadores)
+
+
+
+
+print(gente.text)
 
 if __name__ == "__main__":
     main()
